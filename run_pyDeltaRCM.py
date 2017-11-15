3a2482841731c25ccc52f21c6969272e9f568909
Kang Wang <wangkang1984@gmail.com>
2017-11-14 18:27:55 -0700

    modify test run

--- run_pyDeltaRCM.py
+++ run_pyDeltaRCM.py
@@ -5,9 +5,10 @@ if __name__ == '__main__':
     delta = BmiDelta()
     delta.initialize('deltaRCM.yaml')
     
+    print 'Qw0:', delta._delta.Qw0, ' m-3/s'
+    print 'Qs0:', delta._delta.Qs0, ' m-3/s'
+    print 'Timestep:', delta._delta.dt, ' s'
 
-    for time in range(0,1000):
-        print 'Timestep:', delta.get_time_step()
-        delta.update()
+    for time in range(0,2):
         
-        
+        delta.update()    
